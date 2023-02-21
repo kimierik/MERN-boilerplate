6886f9874d55e2ff75ff3798be95d832184cadf9
kimierik <kimi.malkamaki@gmail.com>
2023-02-21 18:12:16 +0200

    cleanup

--- backend/app.ts
+++ backend/app.ts
@@ -1,1 +1,35 @@
-console.log("server started");
+
+//const express=require('express');
+import express from "express";
+const http=require('http');
+
+const {MongoClient,GridFSBucket, ObjectId } = require('mongodb');
+
+const app=express();
+//const server= http.createServer(app);
+const PORT=3000;
+
+//needed to not trigger corse
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+//example for api endpoint
+app.get('/api',function(req,res,next){
+    res.send("this is a responce from the api");
+});
+
+app.get('/',(req,res)=>{
+    res.send("this is an api");
+});
+
+
+
+
+app.listen(PORT,()=>{
+    console.log('backend on port listening: ',PORT);
+});
--- frontend/MERNapp_frontend/src/App.tsx
+++ frontend/MERNapp_frontend/src/App.tsx
@@ -1,32 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	
+	const [msg,setmsg]=useState("press the button");
+
+  
+	//example on how to send getrqs to backend. this should be some variable tbh
+	async function get(){
+		const res=await fetch("http://localhost:3000/api");
+		const data=await res.text();
+		setmsg(data);
+	}
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+        <button onClick={() =>{ get()} }>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+		<p>
+			{msg}
+		</p>
+
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
