1e93f27a81a314fbc84a9603a3a8dc45a737d2e8
kimierik <kimi.malkamaki@gmail.com>
2023-05-05 11:01:45 +0300

    cleanup

--- backend/app.ts
+++ backend/app.ts
@@ -1,24 +1,17 @@
-
-//const express=require('express');
 import express from "express";
 import bodyParser from "body-parser";
 
-//const formidable=require('formidable');
 import formidable,{errors as formidableErrors} from 'formidable';
-import { Readable } from "stream";
 
 import {MongoClient,GridFSBucket, ObjectId } from 'mongodb';
-import {type} from "os";
 import {createReadStream, readFileSync} from "fs";
+
 
 const app=express();
 const PORT=3000;
 
 const dbname="files"
-
 const mongodbUrl ='mongodb://db:27017'
-
-
 
 //needed to not trigger corse
 app.use(function(_req, res, next) {
@@ -41,7 +34,7 @@ async function main(){
 	await client.connect();
 	console.log('mongo db has connected');
 	const db =client.db(dbname);
-	const collection=db.collection('documents');
+	//const collection=db.collection('documents');
 	const bucket = new GridFSBucket(db,{bucketName:'FileBucket' });
 
 
@@ -74,20 +67,14 @@ async function main(){
     });
 
 
-            // @ts-ignore
-    function getsingle(inp:formidable.File|formidable.File[]):formidable.File{
-        if (inp instanceof formidable.File){
-            // @ts-ignore
-            return inp;
-        }
-    }
-
 
     //send data to the database
     app.post('/api-p',function(req,res,next){
+        //TODO 
+        //currently cannot handle multiple files at a time
 
         const form=formidable({multiples:true});
-        form.parse(req,(err,fields,files)=>{
+        form.parse(req,(err,_fields,files)=>{
             if(err){
                 next(err);
                 return;
@@ -103,24 +90,14 @@ async function main(){
 				metadata: { Category: 'test',Extention : 'test'  }
 			}))
 
-/*
-            // @ts-ignore
-            const stream = Readable.from();O
-            readFileSync(files.file.)
-            
-			stream.
-            console.log("fomind ",fields) ;
-            // @ts-ignore
-            console.log("fil ",files.file.filepath) ;
-                */
             res.send("success");
         })
-        //console.log( req.body);
 
 
-        //res.send("done");
     });
 
+
+    //returns promise with all data from db
 	async function GetAllBDData(){
 		const cursor = bucket.find({});
 		return cursor.map(function(doc){return doc}).toArray();
@@ -130,33 +107,7 @@ async function main(){
     //get all files in the database
     app.get('/',(_req,res)=>{
         GetAllBDData().then((db )=>{ res.send(db); });
-
-        /*
-        const items=[
-            {
-            _id:  "63eb6bc9079bc00e5e5c1b5d",
-            length: 1545347,
-            chunkSize: 1048576,
-            uploadDate: '2023-02-14T11:08:57.382Z',
-            filename: 'hot pic.jpg',
-            metadata: { SenderId: 'asdf1234adfg', Extention: 'jpg',Permission:1 }
-            },
-            {
-            _id: "6fasddfg3easdf12245ac1b5d",
-            length: 1545347,
-            chunkSize: 1048576,
-            uploadDate: '2023-02-14T11:08:57.382Z',
-            filename: 'crazy image.jpg',
-            metadata: { SenderId: 'asdf12lköudfg', Extention: 'jpg',Permission:2 }
-            }
-        ]
-            * */
-       // rhies.send(items);
     });
-
-
-
-
 
 
     app.listen(PORT,()=>{
